2dc31a2957ed3097f57eed84539d49d7909132ee
Shardul Srivastava <shardul.srivastava@iproperty.com>
2018-04-10 12:33:30 +0800

    SumoLogic Lambda Function Integration

--- scripts/frontend-switch-elb.sh
+++ scripts/frontend-switch-elb.sh
@@ -6,4 +6,5 @@ set -ex
 
 docker run --rm -v $(pwd):/opt/deployment -w /opt/deployment ${DOCKER_REGISTRY_URL} \
   ansible-playbook -i inventory/${ENVIRONMENT_INT}/${REGION_NAME}/inventory -vvvv frontend-alb-switch.yml \
-  -e  new_stack_name=${STACK_NAME}
+  -e new_stack_name=${STACK_NAME} \
+  -e version="${VERSION}"
